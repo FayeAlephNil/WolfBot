3def048208fa181c32aadf47023c4f6534cc9adf
Strikingwolf <Strikingwolf2012@gmail.com>
2014-12-08 21:37:53 -0600

    Start work on WolfBot

---
+++ src/WolfBot/Bot.pm
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use diagnostics;
+
+package WolfBot::Bot;
+use base qw(Bot::BasicBot);
+
+
+#My said subroutine
+sub said {
+  #get some args
+  my ($self, $message) = @_;
+  my $body = $message->{body};
+
+  #check if body is a WolfBot command
+  if ($body =~ m/^~/) {
+    #get the command
+    my $command = split(/~/, $body);
+
+    #quit command
+    if ($command eq 'quit') {
+      $self->shutdown();
+    }
+
+    #say command
+    if ($command =~ m/^say/) {
+      #get what to say
+      my $what_to_say = split(\s);
+
+      #say it
+      $self->say(
+        channel => $message->{channel},
+        body    => $what_to_say
+      );
+    }
+  }
+}
+1;
