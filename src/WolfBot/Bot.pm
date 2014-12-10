e3e17b165e681dbbf0f717a01481e7eef7b6d5fe
Strikingwolf <Strikingwolf2012@gmail.com>
2014-12-09 19:44:28 -0600

    More refactoring and change character to *

--- src/WolfBot/Bot.pm
+++ src/WolfBot/Bot.pm
@@ -11,39 +11,54 @@ sub said {
   my ($self, $message) = @_;
   my $body = $message->{body};
 
-  #quit command
-  if ($body eq '~quit') {
-    $self->shutdown();
-  }
+  if ($body =~ m/^\*/) {
+    my ($activation, $command) = split(/^\*/, $body);
 
-  #say command
-  if ($body =~ m/^~say/) {
-    #get what to say
-    my ($say, $what_to_say) = split(/^~say\s/, $body);
+    #quit command
+    if ($command eq 'quit') {
+      $self->shutdown();
+    }
 
-    #say it
-    $self->say(
+    #say command
+    if ($command =~ m/^say/) {
+      #get what to say
+      my ($say, $what_to_say) = split(/^say\s/, $command);
+
+      #say it
+      $self->say(
       channel => $message->{channel},
       body    => $what_to_say
-    );
+      );
+    }
+
+    #kill command
+    if ($command =~ m/^kill/) {
+      my ($kill, $what_to_kill) = split(/^kill\s/, $command);
+
+      $self->say(
+      channel => $message->{channel},
+      body    => ('I have terminated ' . $what_to_kill)
+      );
+    }
+
+    #help command
+    if ($command eq 'help') {
+      $self->say(
+      channel => $message->{channel},
+      body    => ('My activation character is * and I can do these commands: help, say, kill, modemteth, and quit')
+      );
+    }
+
+    #The modemteth command
+    if ($command =~ m/^modemteth/) {
+      my ($modemteth, $what_to_do_with_teth) = split(/modemteth\s/, $command);
+
+      $self->say(
+      channel => $message->{channel},
+      body    => '/me ' . $what_to_do_with_teth
+      );
+    }
   }
 
-  #kill command
-  if ($body =~ m/^~kill/) {
-    my ($kill, $what_to_kill) = split(/^~kill\s/, $body);
-
-    $self->say(
-    channel => $message->{channel},
-    body    => ('I have terminated ' . $what_to_kill)
-    );
-  }
-
- #help command
- if ($body =~ m/~help/) {
-   $self->say(
-   channel => $message->{channel},
-   body    => ('My activation character is ~ and I can do these commands: help, say, kill, and quit')
-   );
- }
 }
 1;
